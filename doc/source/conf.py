62c22a782f71b025c19a10220f01a27e41eae64d
Chris Malek <cmalek@caltech.edu>
2023-05-03 10:41:24 -0700

    Bump version: 1.1.0 → 1.1.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ copyright = '2022, Caltech IMSS ADS'  # pylint: disable=redefined-builtin
 author = 'Caltech IMSS ADS'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="django-sphinx-hosting",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
