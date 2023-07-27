6c1e7f3d38b098ade09f74c1d768e40766e5aeaf
Chris Malek <cmalek@caltech.edu>
2023-07-27 13:15:11 -0700

    Bump version: 1.1.5 → 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="django-sphinx-hosting",
-    version="1.1.5",
+    version="1.2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
