3464d923ce0e045de0a39be6971dbcea3f0837f0
Chris Malek <cmalek@caltech.edu>
2024-08-01 09:58:08 -0700

    Bump version: 1.5.1 → 1.5.2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,7 @@ copyright = '2022, Caltech IMSS ADS'  # pylint: disable=redefined-builtin
 author = 'Caltech IMSS ADS'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5.1'
+release = '1.5.2'
 
 
 # -- General configuration ---------------------------------------------------
